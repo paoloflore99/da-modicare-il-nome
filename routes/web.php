11f8df97afcf56b47e78eaf5d0548e819077080a
paoloflore99 <paoloflore937@gmail.com>
2023-10-02 16:59:58 +0200

    finito

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\Guest\PageController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
 Route::get('/', function () {
-    return view('welcome');
+    $film = \App\Models\Movie::all();
+    return view('welcome', ['film' => $film]);
 });
+
+// Route::get('/movies', [PageController::class, 'index']);
+
+
